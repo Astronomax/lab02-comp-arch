92d01c56f5e4778ea67b1e0bae4a532d74639ec7
Astronomax <fgfgfb93@gmail.com>
2021-11-03 14:46:19 +0300

    Add template arguments
    template<unsigned int WAYS_P, unsigned int LINES_P>
    class lru_for_elements;

--- function.h
+++ function.h
@@ -3,7 +3,57 @@
 
 #include "lru_for_elements.h"
 #include <string>
+#include <iostream>
 
-void f(lru_for_elements &cache, int n, double asqr);
+template<unsigned int WAYS, unsigned int LINES>
+double read_double(lru_for_elements<WAYS, LINES> &cache, unsigned int ind) {
+    double result = 0;
+    uint8_t buffer[8];
+    for (int i = 0; i < 8; i++)
+        buffer[i] = cache.read(ind + i);
+    memcpy(&result, buffer, sizeof(double));
+    return result;
+}
+
+template<unsigned int WAYS, unsigned int LINES>
+void write_double(lru_for_elements<WAYS, LINES> &cache, unsigned int ind, double new_val) {
+    uint8_t buffer[8];
+    memcpy(buffer, &new_val, sizeof(double));
+    for (int i = 0; i < 8; i++)
+        cache.write(ind + i, buffer[i]);
+}
+
+template<unsigned int WAYS, unsigned int LINES>
+double read_element_field(lru_for_elements<WAYS, LINES> &cache, unsigned int ind, const std::string& field_name) {
+    std::string element_field[8] = {"x", "y", "ax", "ay", "vx", "vy", "a", "b"};
+    for(int i = 0; i < 8; i++)
+        if(element_field[i] == field_name)
+            return read_double(cache, 64 * ind + i * 8);
+    return 0;
+}
+
+template<unsigned int WAYS, unsigned int LINES>
+void write_element_field(lru_for_elements<WAYS, LINES> &cache, unsigned int ind, const std::string& field_name, double new_val) {
+    std::string element_field[8] = {"x", "y", "ax", "ay", "vx", "vy", "a", "b"};
+    for(int i = 0; i < 8; i++)
+        if(element_field[i] == field_name)
+            write_double(cache, 64 * ind + i * 8, new_val);
+}
+
+template<unsigned int WAYS, unsigned int LINES>
+void f(lru_for_elements<WAYS, LINES> &cache, int n, double asqr) {
+    for (int i = 0; i < n; ++i) {
+        double x = read_element_field(cache, i, "x"),
+                ax = read_element_field(cache, i, "ax"),
+                vx = read_element_field(cache, i, "vx");
+        write_element_field(cache, i, "x", x + vx * asqr + 0.5 * ax * asqr * asqr);
+    }
+    for (int i = 0; i < n; ++i) {
+        double y = read_element_field(cache, i, "y"),
+                ay = read_element_field(cache, i, "ay"),
+                vy = read_element_field(cache, i, "vy");
+        write_element_field(cache, i, "y", y + vy * asqr + 0.5 * ay * asqr * asqr);
+    }
+}
 
 #endif //UNTITLED1_FUNCTION_H
--- lru_for_elements.h
+++ lru_for_elements.h
@@ -8,25 +8,72 @@ struct element {
     double x, y, ax, ay, vx, vy, a, b;
 };
 
+template<unsigned int WAYS_P, unsigned int LINES_P>
 class lru_for_elements {
 public:
-    explicit lru_for_elements(uint8_t *ram);
-    uint8_t read(unsigned int ind);
-    void write(unsigned int ind, uint8_t new_val);
-    void remove_line(unsigned int way, unsigned int line) const;
-    void clear() const;
-    unsigned int get_queries() const;
-    unsigned int get_misses() const;
-
+    explicit lru_for_elements(uint8_t* _ram) {
+        memset(&_size[0], 0, LINES);
+        memset(&tags[0][0], 0, LINES * WAYS);
+        memset(&ruse[0][0], 0, LINES * WAYS);
+        memset(&cache[0][0][0], 0, WAYS * LINES * 64);
+        ram = _ram;
+    }
+    uint8_t read(unsigned int ind) {
+        return get(ind);
+    }
+    void write(unsigned int ind, uint8_t new_val) {
+        get(ind) = new_val;
+    }
+    void remove_line(unsigned int way, unsigned int line) const {
+        unsigned int rm_tag = (((tags[line][way] << LINES_P) + line) << 6u);
+        memcpy(ram + rm_tag, cache[way][line], 64);
+    }
+    void clear() const {
+        for (unsigned int line = 0; line < LINES; line++)
+            for (unsigned int way = 0; way < _size[line]; way++)
+                remove_line(way, line);
+    }
+    unsigned int get_queries() const {
+        return queries;
+    }
+    unsigned int get_misses() const {
+        return cache_misses;
+    }
 private:
-    uint8_t& get(unsigned int ind);
+    uint8_t& get(unsigned int ind)  {
+        ++queries;
+        unsigned int line = (ind >> 6u) & ((1u << LINES_P) - 1u);
+        unsigned int _tag = (ind >> (LINES_P + 6u));
+        unsigned int byte_ind = (ind & ((1u << 6u) - 1u));
 
+        for (int i = 0; i < _size[line]; i++) {
+            if (tags[line][i] == _tag) {
+                ruse[line][i] = queries;
+                return cache[i][line][byte_ind];
+            }
+        }
+        ++cache_misses;
+        unsigned int way = 0;
+        if (_size[line] < WAYS)
+            way = _size[line]++;
+        else {
+            for (unsigned int i = 0; i < WAYS; i++)
+                if (ruse[line][i] < ruse[line][way])
+                    way = i;
+            remove_line(way, line);
+        }
+        memcpy(cache[way][line], ram + ((ind >> 6u) << 6u), 64);
+        tags[line][way] = _tag;
+        ruse[line][way] = queries;
+        return cache[way][line][byte_ind];
+    }
 private:
-    unsigned int queries = 0, misses = 0;
-    unsigned int _size[128]{};
-    unsigned int ruse[128][4]{};
-    uint8_t addr[128][4]{};
-    uint8_t cache[4][128][64]{};
+    static const unsigned int WAYS = (1 << WAYS_P), LINES = (1 << LINES_P);
+    unsigned int queries = 0, cache_misses = 0;
+    unsigned int _size[LINES]{};
+    unsigned int ruse[LINES][WAYS]{};
+    uint8_t tags[LINES][WAYS]{};
+    uint8_t cache[WAYS][LINES][64]{};
     uint8_t* ram;
 };
 
